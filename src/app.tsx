f382d83e2b58f2cddbd431982a88c8b72c39be33
unknown <james.thomson019@gmail.com>
2022-09-18 20:09:38 +0100

    electron working with react now

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = (): JSX.Element => (
+    
+  <div>
+    {/* <script src="../src/index.tsx"></script> */}
+    <p>😶‍🌫️ Hello there</p>
+    <p>🤖 General Kenobi !</p>
+  </div>
+);
+
+export default App;
